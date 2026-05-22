f4a17ead2f7a972ed514e37fabec62785d5d8f2c
EspejelKevin <kevin.espejelmtz@gmail.com>
2026-05-22 08:28:46 -0600

    feature: utilizacion de un scraping strategy para el scraper

--- src/application/usecases/scraping_task.py
+++ src/application/usecases/scraping_task.py
@@ -10,10 +10,11 @@ class ScrapingTaskUseCase:
         product = self.product_repository.get_by_id(product_id)
 
         try:
-            current_price = await self.scraper.get_price(product.url, product.selector)
+            current_price = await self.scraper.get_price(product.url, product.selector, product.scraping_strategy)
 
             if current_price <= product.target_price:
                 print('Alerta de precio')
+
         except Exception as ex:
             self.product_repository.update_status(product_id, 'error_selector')
             print(f'Error scraping: {ex}')
--- src/domain/interfaces/scraper_service.py
+++ src/domain/interfaces/scraper_service.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class ScraperService(ABC):
     @abstractmethod
-    async def get_price(self, url: str, selector: str) -> float:
+    async def get_price(self, url: str, selector: str, strategy_name: str) -> float:
         raise NotImplementedError
